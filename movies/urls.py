f0b70fcdbab16c16529f2626b05925e0e2a612af
rohmantique <qndjd711@gmail.com>
2022-05-24 14:50:19 +0900

    heejin/ urls, views 수정

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,8 @@ app_name = 'movies'
 urlpatterns = [
     path('selectmood/', views.select_mood, name='select_mood'),
     path('index/<str:mood_pk>/', views.index, name='index'),
-    path('<str:movie_pk>/', views.detail, name='detail'),
-    path('<str:movie_pk>/create/', views.create, name='create'), #리뷰작성
+    path('<str:mood_pk>/<str:movie_pk>/', views.detail, name='detail'),
+    path('create/<str:movie_pk>/', views.create, name='create'), #리뷰작성
+    path('<str:movie_pk>/save/', views.save, name='save'),
     
 ]
